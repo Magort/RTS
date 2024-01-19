e44ac0f8c9e563d40bac30be8236c7211d029d81
M K <michal.kostecki@op.pl>
2024-01-19 14:10:12 +0100

    Project Prefs restored

--- Assets/Scripts/DebugStick.cs
+++ Assets/Scripts/DebugStick.cs
@@ -1,13 +1,22 @@
+using TMPro;
 using UnityEngine;
 
 public class DebugStick : MonoBehaviour
 {
     public MapUnit unit;
+    public TextMeshProUGUI fpsCounter;
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log(TileGrid.MainTile.coordinates);
-		}
+        //if(Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    Debug.Log(TileGrid.MainTile.coordinates);
+        //}
+
+        UpdateFPS();
+    }
+
+    public void UpdateFPS()
+    {
+        fpsCounter.text = Mathf.RoundToInt(1 / Time.unscaledDeltaTime).ToString();
     }
 }
